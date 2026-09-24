c3ad322d9e4233765c5b5e68c98218dc183c8451
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-09-24 12:59:46 +0200

    rewrite of snippet

--- after/plugin/snippets/swift/swift-package-warnings-as-errors.lua
+++ after/plugin/snippets/swift/swift-package-warnings-as-errors.lua
@@ -4,53 +4,63 @@ if not ok then
     return
 end
 
-local snippet = luasnip.snippet
-local text_node = luasnip.text_node
-local insert_node = luasnip.insert_node
-
 local function is_package_swift()
     return vim.fn.expand("%:t") == "Package.swift"
 end
 
-luasnip.add_snippets("swift", {
-    snippet(
-        {
-            trig = "spmwarn",
-            name = "Treat all SwiftPM warnings as errors",
-            dscr = "Apply treatAllWarnings(as: .error) to applicable package targets",
-        },
-        {
-            text_node(
-                {
-                    "for target in package.targets {",
-                    "    switch target.type {",
-                    "    case .regular, .executable, .test, .macro:",
-                    "        var settings = target.swiftSettings ?? []",
-                    "",
-                    "        settings.append(",
-                    "            .treatAllWarnings(as: .error)",
-                    "        )",
-                    "",
-                    "        target.swiftSettings = settings",
-                    "",
-                    "    case .plugin, .system, .binary:",
-                    "        break",
-                    "",
-                    "    @unknown default:",
-                    "        break",
-                    "    }",
-                    "}",
-                    "",
-                }
-            ),
-            insert_node(0),
-        },
-        {
-            condition = is_package_swift,
-            show_condition = is_package_swift,
-        }
-    ),
-    }, {
-        key = "levi-swift-package-snippets",
+-- input args:
+-- luasnip.add_snippets(filetype, snippets, options)
+
+local filetype = "swift"
+
+-- single snippet object:
+local warning_as_errors_snippet = luasnip.snippet(
+    {
+        trig = "warnings_as_errors_package",
+        name = "Treat all SwiftPM warnings as errors",
+        dscr = "Apply treatAllWarnings(as: .error) to applicable package targets",
+    },
+    {
+        luasnip.text_node(
+            {
+                "for target in package.targets {",
+                "    switch target.type {",
+                "    case .regular, .executable, .test, .macro:",
+                "        var settings = target.swiftSettings ?? []",
+                "",
+                "        settings.append(",
+                "            .treatAllWarnings(as: .error)",
+                "        )",
+                "",
+                "        target.swiftSettings = settings",
+                "",
+                "    case .plugin, .system, .binary:",
+                "        break",
+                "",
+                "    @unknown default:",
+                "        break",
+                "    }",
+                "}",
+                "",
+            }
+        ),
+        luasnip.insert_node(0),
+    },
+    {
+        condition = is_package_swift,
+        show_condition = is_package_swift,
     }
 )
+
+-- snippets collected
+local snippets = {
+    warning_as_errors_snippet,
+}
+
+-- options
+local options = {
+    key = "levi-swift-package-snippets",
+}
+
+-- final call
+luasnip.add_snippets(filetype, snippets, options)
